1d6aab34ffb654a45afec243f3dad8748bf7e5e5
ena <teankim2@gmail.com>
2026-07-11 10:27:46 +0900

    add main menu scene changing

--- objects/ui/menus/main_menu/main_menu.gd
+++ objects/ui/menus/main_menu/main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 # Main menu script that controls entire main menu. 
 # Settings menu is implemented through its own script though
 
+@export_file("*.tscn") var game_scene
 
 @export var play_button : Button
 @export var settings_button : Button
@@ -16,7 +17,7 @@ func _ready() -> void:
 
 
 func _on_play_button_pressed() -> void:
-  pass
+  get_tree().change_scene_to_file(game_scene)
 
 
 func _on_settings_button_pressed() -> void:
