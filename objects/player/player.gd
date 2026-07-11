94db9fcb22b50facc7df0beed1ec527530f21a9d
ena <teankim2@gmail.com>
2026-07-11 23:02:05 +0900

    add coyote time and jump buffer time

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -1,6 +1,7 @@
 class_name Player
 extends CharacterBody2D
 # A base player class
+# Probably should move a lot of the logic into separate components
 
 @export_category("Movement Variables")
 @export var speed : float = 10.0
@@ -10,19 +11,49 @@ extends CharacterBody2D
 
 @export_category("Node References")
 @export var visuals : Node2D
+@export var coyote_timer : Timer
+@export var jump_buffer_timer : Timer
 
 @onready var jump_velocity : float = (( 2.0 * jump_height) / jump_time_to_peak) * -1.0
 @onready var jump_gravity  : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
 @onready var fall_gravity  : float = ((-2.0 * jump_height) / (jump_time_to_fall * jump_time_to_fall)) * -1.0
 
+var _was_on_floor : bool = true
+
 
 func _physics_process(delta: float) -> void:
   velocity.x = Input.get_axis("left", "right") * speed
-  if Input.is_action_just_pressed("jump") && is_on_floor():
-    velocity.y += jump_velocity
   velocity.y += _get_gravity() * delta
+  _handle_jump()
 
   move_and_slide()
+
+
+func _handle_jump() -> void:
+  if is_on_floor():
+    if _was_on_floor == false:
+      _was_on_floor = true
+    if Input.is_action_just_pressed("jump") || (jump_buffer_timer.time_left > 0.0 && Input.is_action_pressed("jump")):
+      _jump()
+
+  else:
+    if _was_on_floor == true:
+      _was_on_floor = false
+      coyote_timer.start()
+    if Input.is_action_just_pressed("jump") :
+      if coyote_timer.time_left > 0.0:
+        _jump()
+      else:
+        jump_buffer_timer.start()
+
+
+func _jump() -> void:
+  _was_on_floor = false
+  velocity.y = jump_velocity
+  if coyote_timer.time_left > 0.0:
+    coyote_timer.stop()
+  if jump_buffer_timer.time_left > 0.0:
+    jump_buffer_timer.stop()
 
 
 func _process(_delta: float) -> void:
