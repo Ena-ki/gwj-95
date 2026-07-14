5d7cbeb9e2d4cdeb7958a996773a82943645637a
ena <teankim2@gmail.com>
2026-07-14 16:35:37 +0900

    add level selector

---
+++ objects/etc/player_data.gd
@@ -0,0 +1,11 @@
+class_name PlayerData
+extends Resource
+
+signal level_progress_updated(level_progress : int)
+
+@export var level_progress : int = 1:
+  set(val):
+    level_progress = val
+    level_progress_updated.emit(val)
+  get:
+    return level_progress
