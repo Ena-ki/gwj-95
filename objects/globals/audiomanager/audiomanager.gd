01a5828bbed215665fd45b71f82d95f0dc5ee1f1
Jecht <devjecht@gmail.com>
2026-07-15 14:37:04 -0700

    audiomanager stuff
    should be able to do anything

--- objects/globals/audiomanager/audiomanager.gd
+++ objects/globals/audiomanager/audiomanager.gd
@@ -11,6 +11,9 @@ var sfx_pool: Array[AudioStreamPlayer2D] = []
 var sfx_count: int = 12
 var sfx_ind: int = 0
 
+var music_tween: Tween
+
+
 func _ready() -> void:
   music_player = AudioStreamPlayer2D.new()
   music_player.bus = MUSIC_BUS
@@ -33,5 +36,50 @@ func play_sfx(stream: AudioStreamPlayer2D, pitch: float = 1.0, volume: float = 1
   player.volume_db = volume
   player.play()
   
-func play_music():
-  pass
+func play_music(stream: AudioStream, fade: float = 1.0):
+  if stream == curr_music:
+    return
+  
+  if music_tween:
+    music_tween.kill()
+  
+  if music_player.playing:
+    music_tween = create_tween()
+    music_tween.tween_property(music_player, "volume_db", -40.0, fade)
+    music_tween.tween_callback(func(): start_new_track(stream, fade))
+  else:
+    start_new_track(stream, fade)
+      
+      
+func start_new_track(stream: AudioStream, fade: float = 1.0):
+  music_player.stream = stream
+  music_player.volume_db = -40.0
+  music_player.play()
+  
+  music_tween = create_tween()
+  music_tween.tween_property(music_player, "volume_db", 0, fade)
+  
+  
+func stop_music(fade: float = 1.0):
+  if not music_player.playing:
+    return
+  
+  if music_tween:
+    music_tween.kill()
+  music_tween = create_tween()
+  music_tween.tween_property(music_player, "volume_db", -40.0, fade)
+  music_tween.tween_callback(music_player.stop)
+  curr_music = null
+  
+func set_bus_vol(busName: String, vol: float):
+  var idx := AudioServer.get_bus_index(busName)
+  if idx != -1:
+    AudioServer.set_bus_volume_db(idx, vol)
+
+func mute_bus(busName: String, mute: bool):
+  var idx := AudioServer.get_bus_index(busName)
+  if idx != -1:
+    AudioServer.set_bus_mute(idx, mute)
+
+
+  
