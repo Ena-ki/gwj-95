3e158856a4f5e2a4bb1454fe935bcd9d5b67418e
ena <teankim2@gmail.com>
2026-07-18 03:53:24 +0900

    add low pass filter while inputing

--- objects/abilities/ability_input_overlay/ability_input_overlay.gd
+++ objects/abilities/ability_input_overlay/ability_input_overlay.gd
@@ -34,22 +34,22 @@ func _process(_delta: float) -> void:
 
 func _on_line_edit_text_changed(text : String) -> void:
   if text.length() > 3:
-    AudioLoader.play_sound(wrong_code_sound)
+    AudioLoader.play_sound(wrong_code_sound, 1.0, true)
     _exit_editing()
     return
-  AudioLoader.play_sound(input_sound)
+  AudioLoader.play_sound(input_sound, -2.0, true)
 
   if text == dash_code_left:
     dash.dash_left()
-    AudioLoader.play_sound(correct_code_sound)
+    AudioLoader.play_sound(correct_code_sound, 1.0, true)
     _exit_editing()
   if text == dash_code_right:
     dash.dash_right()
-    AudioLoader.play_sound(correct_code_sound)
+    AudioLoader.play_sound(correct_code_sound, 1.0, true)
     _exit_editing()
   if text == jump_code:
     big_jump.big_jump()
-    AudioLoader.play_sound(correct_code_sound)
+    AudioLoader.play_sound(correct_code_sound, 1.0, true)
     _exit_editing()
 
 
@@ -58,9 +58,11 @@ func _start_editing() -> void:
   visible = true
   line_edit.text = ""
   Engine.time_scale = 0.1
+  AudioServer.set_bus_effect_enabled(1, 0, true)
 
 
 func _exit_editing() -> void:
   Engine.time_scale = 1.0
+  AudioServer.set_bus_effect_enabled(1, 0, false)
   line_edit.unedit()
   visible = false
--- objects/globals/audio_loader/audio_loader.gd
+++ objects/globals/audio_loader/audio_loader.gd
@@ -3,22 +3,26 @@ extends Node
 var current_music: AudioStreamPlayer
 
 
-func play_sound(audio : AudioStream, volume := 0.0):
+func play_sound(audio : AudioStream, volume := 0.0, master_bus : bool = false):
   var player := AudioStreamPlayer.new()
   get_tree().current_scene.add_child(player)
   player.volume_db = volume
   player.stream = audio
+  if !master_bus:
+    player.bus = "Pitched"
   player.playing = true
   player.finished.connect(player.queue_free)
 
 
-func play_music(audio : AudioStream, volume := 0.0) -> void:
+func play_music(audio : AudioStream, volume := 0.0, master_bus : bool = false) -> void:
   if current_music:
     current_music.queue_free()
   current_music = AudioStreamPlayer.new()
   add_child(current_music)
   current_music.volume_db = volume
   current_music.stream = audio
+  if !master_bus:
+    current_music.bus = "Pitched"
   current_music.playing = true
   current_music.finished.connect(current_music.queue_free)
 
