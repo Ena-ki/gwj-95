f02988b586bbecfc1d7e607c149eb007a7480193
ena <teankim2@gmail.com>
2026-07-18 23:05:11 +0900

    fix color palette on startup

--- objects/ui/menus/settings_menu/settings_items/settings_color_item.gd
+++ objects/ui/menus/settings_menu/settings_items/settings_color_item.gd
@@ -3,16 +3,19 @@ extends OptionButton
 
 @export var shader : ShaderMaterial
 @export var palettes : Array[ColorPaletteItem]
+@export var player_data : PlayerData
 
 
 func _ready() -> void:
   item_selected.connect(_on_item_selected)
   for i in range(palettes.size()):
     add_item(palettes[i].palette_name, i)
+  select(player_data.current_palette_id)
 
 
 func _on_item_selected(i : int) -> void:
-  var item := palettes[i]
+  player_data.current_palette = palettes[i]
+  player_data.current_palette_id = i
   print(get_item_text(i))
-  print(palettes[i-1].palette_name)
-  shader.set_shader_parameter("color_map", item.palette)
+  print(palettes[i].palette_name)
+  shader.set_shader_parameter("color_map", player_data.current_palette.palette)
