4ccadd53c92039ea566483cc1f62e33a151f95fa
Jecht <devjecht@gmail.com>
2026-07-18 20:01:32 -0700

    player

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -97,8 +97,8 @@ func _visual_flip() -> void:
 func _get_gravity() -> float:
   return jump_gravity if velocity.y < 0.0 else fall_gravity
 
-func die():
-  SpawnpointManager.respawn_player(self)
-  
+#func die():
+ # SpawnpointManager.respawn_player(self)
+  # FUUUUUUUUUUUUUUUUUUUCKKKKKKKKKKKKKKKKKK
 # add more here if you'd like
   
